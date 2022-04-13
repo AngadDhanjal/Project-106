b35de313b4b0f0926dce751b6f4b214651ee1337
AngadDhanjal <88576513+AngadDhanjal@users.noreply.github.com>
2022-04-13 22:06:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 //https://teachablemachine.withgoogle.com/models/Nl0878igW/
+
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/Nl0878igW/model.json',modelReady)
+}
+
+function modelReady()
+{
+    classifier.classfify(gotResults);
+}
